764cc1d23e94da82300296bfead5e108b205bbb4
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2023-07-09 01:02:18 +0530

    Update web-server.sh

--- web-server.sh
+++ web-server.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Function to install packages
-install_packages() {
-    # Install Caddy
+echo "Select an option:"
+echo "1. Install services"
+echo "2. Remove services"
+
+read -p "Enter your choice (1 or 2): " choice
+
+if [[ $choice == 1 ]]; then
+    # Option 1: Install services
     echo "Installing Caddy..."
     sudo apt update
     sudo apt upgrade -y
@@ -15,7 +20,6 @@ install_packages() {
     sudo systemctl stop caddy
     echo "Caddy installation completed."
 
-    # Install Nginx
     echo "Installing Nginx..."
     sudo apt update
     sudo apt install -y nginx
@@ -23,7 +27,6 @@ install_packages() {
     sudo systemctl stop nginx
     echo "Nginx installation completed."
 
-    # Install Apache2
     echo "Installing Apache2..."
     sudo apt update
     sudo apt install -y apache2
@@ -31,44 +34,28 @@ install_packages() {
     sudo systemctl stop apache2
     echo "Apache2 installation completed."
 
-    # Check service status
     echo "Checking service status..."
     sudo systemctl status nginx
     sudo systemctl status caddy
     sudo systemctl status apache2
-}
 
-# Function to remove packages
-remove_packages() {
-    # Remove Caddy
+elif [[ $choice == 2 ]]; then
+    # Option 2: Remove services
     echo "Removing Caddy..."
     sudo apt remove -y caddy
     sudo rm /usr/share/keyrings/caddy-stable-archive-keyring.gpg
     sudo rm /etc/apt/sources.list.d/caddy-stable.list
     echo "Caddy removal completed."
 
-    # Remove Nginx
     echo "Removing Nginx..."
     sudo apt remove -y nginx
     echo "Nginx removal completed."
 
-    # Remove Apache2
     echo "Removing Apache2..."
     sudo apt remove -y apache2
     echo "Apache2 removal completed."
-}
 
-# Read user input
-read -p "Enter 1 to install packages or 2 to remove packages: " choice
-
-# Execute script based on user input
-if [ "$choice" == "1" ]; then
-    install_packages
-elif [ "$choice" == "2" ]; then
-    remove_packages
 else
     echo "Invalid choice. Exiting..."
     exit 1
 fi
-
-
