6dd7aac11d302d955408ce06ff548ede5966ffef
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2023-07-12 22:27:33 +0530

    Update ZeroTier.sh

--- ZeroTier.sh
+++ ZeroTier.sh
@@ -1,36 +1,59 @@
 #!/bin/bash
 echo "Application link"
-echo "https://snapcraft.io/install/zerotier/ubuntu"
+echo "https://snapcraft.io/install/zerotier/ubuntu
+echo "Select an option:"
+echo "1. Install services"
+echo "2. Remove services"
 
-sudo apt upgrade -y
-sudo apt update -y
-sudo apt install curl -y
+read -p "Enter your choice (1 or 2): " choice
 
-curl -s https://install.zerotier.com | sudo bash
-curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
-if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+if [[ $choice == 1 ]]; then
+    # Option 1: Install services
 
-# Define the file path and content
-file_path="/var/lib/zerotier-one/local.conf"
-file_content='{ "settings": { "primaryPort": 9994 } }'
+sudo apt update
+sudo apt install snapd
+sudo snap install zerotier
+touch /var/lib/zerotier-one/local.conf
+cd  /var/lib/zerotier-one/
+chmod 777 /var/lib/zerotier-one/local.conf
 
-# Create the file with the specified content
-echo "$file_content" > "$file_path"
-
-# Confirm the file creation
-if [ -e "$file_path" ]; then
-echo "local.conf file created successfully""
-else
-echo  "local.conf file created fail"
-fi
-
-#permistions change
-sudo chmod -R 777  /var/lib/zerotier-one/local.conf/local.conf 
-
-#join blusapphire network
-zerotier-cli join 81b9052ecd4865a9
+# Open local.conf in a text editor
+vi local.conf <<EOF
+{
+  "settings": {
+    "primaryPort": 9994
+  }
+}
+EOF
 
 sudo systemctl restart zerotier-one.service
+#Start ZeroTier in daemon mode
+zerotier-one -d
+echo "Join a Blusapphire-ZeroTier network "
+zerotier-cli join 81b9052ecd4865a9
+sudo apt upgrade
+sudo apt update
+echo "ZeroTier has been installed."
+echo "zerotier-cli info"
 
-echo "please note the address"
-erotier-cli info
+elif [[ $choice == 2 ]]; then
+    # Option 2: Remove services
+	
+sudo systemctl stop zerotier-one.service
+sudo snap remove zerotier
+sudo rm -rf /var/lib/zerotier-one
+rm -rf ~/.zerotier
+sudo rm -rf /etc/systemd/system/zerotier-one.service
+sudo rm -rf /etc/zerotier-one
+sudo rm /usr/bin/zerotier-cli
+sudo rm /usr/sbin/zerotier-idtool
+sudo rm /usr/sbin/zerotier-one
+sudo apt upgrade
+sudo apt update 
+echo "ZeroTier has been removed."
+
+else
+    echo "Invalid choice. Exiting..."
+    exit 1
+fi
+
