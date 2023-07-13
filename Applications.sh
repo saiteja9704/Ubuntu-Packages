f6c6bae311003ca563318e7a8df6f33c027781d9
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2023-07-13 14:20:46 +0530

    Update Applications.sh

--- Applications.sh
+++ Applications.sh
@@ -36,12 +36,12 @@ if [ $choice -eq 1 ]; then
   sudo apt update && sudo apt install microsoft-edge-stable
   echo "microsoft-edge installed successfully."
 
-#  sudo apt install curl
-#  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-#  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-#  sudo apt update
-#  sudo apt install brave-browser
-#  echo " Brave-browser installed successfully."
+  sudo apt install curl
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+  sudo apt install brave-browser
+  echo " Brave-browser installed successfully."
 
   sudo snap install unofficial-webapp-todo 
   sudo snap install office365webdesktop --beta
